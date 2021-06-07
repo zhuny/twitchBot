a4bd14c4b3ffb8951450eb3f03f17fbb83159e0d
zhuny <zhuny@kaist.ac.kr>
2021-06-08 00:28:34 +0900

    map to level

--- command/smm2.js
+++ command/smm2.js
@@ -1,4 +1,4 @@
-const userRegisterMap = [];
+const userRegisterLevel = [];
 
 const isValidLevelCode = (levelCode) => {
   return (
@@ -14,29 +14,29 @@ export const initSMM2 = (commandMap) => {
   commandMap.add = ({say, context: {username}, args: [levelCode]}) => {
     // Level Code 확인하기
     if (!isValidLevelCode(levelCode)) {
-      say(`@${username}, Incorrect command. Use !add {level-code}`);
+      say(`@${username}, Incorrect level code. Use !add {level-code}`);
       return;
     }
 
     // 이전에 코드를 등록한 적 있는지 확인하기
-    const registerMap = userRegisterMap.find((m) => m.username === username);
-    if (registerMap !== undefined) {
-      say(`@${username}, You already added ${registerMap.levelCode}`);
+    const registerLevel = userRegisterLevel.find((m) => m.username === username);
+    if (registerLevel !== undefined) {
+      say(`@${username}, You already added ${registerLevel.levelCode}`);
       return;
     }
 
     // 맵 코드 등록하기
     levelCode = levelCode.toUpperCase();
-    userRegisterMap.push({username, levelCode});
+    userRegisterLevel.push({username, levelCode});
     say(`@${username}, Thx for adding. Your level code : ${levelCode}`);
   };
 
   commandMap.remove = ({say, context: {username}, args: [levelCode]}) => {
-    const registerLevelIndex = userRegisterMap.findIndex((m) => m.username === username);
+    const registerLevelIndex = userRegisterLevel.findIndex((m) => m.username === username);
     if (registerLevelIndex === -1) {
       say(`@${username}, You didn't registry any level.`);
     } else {
-      const removedLevel = userRegisterMap.splice(registerLevelIndex, 1)[0];
+      const removedLevel = userRegisterLevel.splice(registerLevelIndex, 1)[0];
       say(`@${username}, Your level ${removedLevel.levelCode} is removed`)
     }
   };
