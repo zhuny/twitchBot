2f4926abf0bade0b6040540b6a97c9f00bb4be00
zhuny <zhuny@kaist.ac.kr>
2021-07-28 01:39:18 +0900

    ignore broadcaster

--- command/welcome.js
+++ command/welcome.js
@@ -3,6 +3,11 @@ export const initWelcome = (bot) => {
 
   bot.registerHandler({
     handler: ({say, context: {badges: {broadcaster}, username}}) => {
+      // 방장은 반응 안해주기ㅋㅋ
+      if (broadcaster === '1') {
+        return;
+      }
+
       if (!firstMessageTime[username]) {
         say("Welcome!!! to zhuny's stream!");
         firstMessageTime[username] = new Date();
